ec2ddd834fa1da34bec43b1a6974a1d239f8e74b
Jacob Hollings <hollingsjacob05@gmail.com>
2025-10-03 14:36:15 -0400

    completed exercises 06-11

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let concatString = ""
+    for(let i=0; i<number; i++){
+        concatString+=string
+    }
+    return concatString
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  cel= ((temp-32) * (5/9))
+  return Number(cel.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  far = ((temp*9/5) + 32)
+  return Number(far.toFixed(1))
 };
 
 // Do not edit below this line
